6a38dc10bb07c6c6747255755b0df6be8e5c4da2
Lucas Kunz <lucaskunz1@gmail.com>
2021-10-19 20:53:42 -0300

    Create controllers

---
+++ src/controllers/GetLast3MessageController.ts
@@ -0,0 +1,14 @@
+import { Response } from "express";
+import { GetLast3MessageService } from "../services/GetLast3MessagesService";
+
+class GetLast3MessageController {
+  async handle(response: Response) {
+    const service = new GetLast3MessageService();
+
+    const result = await service.execute();
+
+    return response.json(result);
+  }
+}
+
+export { GetLast3MessageController };
